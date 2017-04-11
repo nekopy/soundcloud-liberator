6f813cf1d28379be0a274d09e07348597792fc85
neko.py <neko@cock.lu>
2017-04-10 23:00:22 -0700

    Greatly Improved Functionality
    1. Usinng originURL was extremely unreliable. Now we properly correlate current page to mp3 filename

--- listener.js
+++ listener.js
@@ -1,21 +1,72 @@
 'use strict';
 
 var targetFormat = "https://cf-hls-media.sndcdn.com/media/*"
+var clientID = null
 
 var soundMap = new Map();
 
+
+function getMP3TrackFromUrl(url) {
+	var mp3Regex = /[a-zA-Z0-9]+\.128\.mp3/;
+	return url.match(mp3Regex)[0];
+}
+
+function xhrForJSON(request, continuation) {
+	var xhr = new XMLHttpRequest();
+	
+	xhr.addEventListener("load", () => {
+		continuation(JSON.parse(xhr.responseText));
+	});
+	xhr.open("GET", request);
+	xhr.send();
+}
+
+function resolveTrackId(currentUrl, resolve) {
+
+	var resolveUrl = "https://api.soundcloud.com/resolve?url=" + currentUrl + "&client_id=" + clientID;
+	xhrForJSON(resolveUrl, (response) => {
+		resolve(response.id);
+	});
+}
+
+function resolveStreamInfo(trackId, resolve) {
+	var streamsUrl = "https://api.soundcloud.com/i1/tracks/" + trackId +"/streams?client_id=" + clientID;
+	
+	xhrForJSON(streamsUrl, (response) => {
+		resolve(getMP3TrackFromUrl(response.hls_mp3_128_url));
+	});	
+}
+
+
 browser.browserAction.onClicked.addListener((e) => {
 	console.log(e.url);
-	console.log("OH FUCK");
-	if (!soundMap.has(e.url))
+	var testURL = e.url.search(/https:\/\/soundcloud\.com/);
+	if (-1 == testURL)
 		return;
-	browser.tabs.create({"active": true, "url": soundMap.get(e.url).url})
+
+	if (null == clientID)
+		return;
+
+	var gettingTrackId = new Promise((resolve, reject) => {
+		resolveTrackId(e.url, resolve);	
+	});
+	gettingTrackId.then((trackId) => {
+		console.log("track Id: ", trackId);
+		var gettingStreamInfo = new Promise((resolve, reject) => {
+			resolveStreamInfo(trackId, resolve);
+		});
+		gettingStreamInfo.then((mp3Track) => {
+			console.log("Track is!!! ", mp3Track);
+			if (soundMap.has(mp3Track))
+				console.log(browser.tabs.create({"active": true, "url": soundMap.get(mp3Track).url}))
+		});
+	});
 });
 
 
-function updateMap(origin, highSample, url) {
-	if (soundMap.has(origin)) {
-		var soundEntry = soundMap.get(origin);
+function updateMap(mp3Filename, highSample, url) {
+	if (soundMap.has(mp3Filename)) {
+		var soundEntry = soundMap.get(mp3Filename);
 		if (soundEntry.highSample >= highSample) {
 			return;
 		}
@@ -24,8 +75,8 @@ function updateMap(origin, highSample, url) {
 		"highSample": highSample,
 		"url": url
 	};
-	console.log("Registering new entry for origin ", origin);
-	soundMap.set(origin, newEntry);
+	console.log("Registering new entry for mp3Filename ", mp3Filename);
+	soundMap.set(mp3Filename, newEntry);
 }
 
 
@@ -44,18 +95,20 @@ function parseMp3Url(trackUrl) {
 	}
 	if (!foundMedia)
 		return retArray;
+	var mp3Filename = getMP3TrackFromUrl(trackUrl);
+	console.log("Filename is ", mp3Filename);
 	var highSample = Number(urlArray[foundMedia + 2]);
 	console.log("high sample number is ", highSample)
 	urlArray[foundMedia + 1] = "0"
 	var downloadUrl = urlArray.join('/');
-	return [highSample, downloadUrl];
+	return [mp3Filename, highSample, downloadUrl];
 }
 
 function registerTrack(responseDetails) {
 	var results = parseMp3Url(responseDetails.url)
-	if (null == results[1])
+	if (null == results[2])
 		return;
-	updateMap(responseDetails.originUrl, results[0], results[1]);
+	updateMap(results[0], results[1], results[2]);
 }
 
 browser.webRequest.onCompleted.addListener(
@@ -63,3 +116,13 @@ browser.webRequest.onCompleted.addListener(
 	{urls: [targetFormat]}
 );
 
+
+function getClientID(responseDetails) {
+	clientID = responseDetails.url.split("client_id=")[1].split('&')[0];
+	console.log("Client ID is ", clientID);
+}
+
+browser.webRequest.onCompleted.addListener(
+	getClientID,
+	{urls: ["https://api.soundcloud.com/i1/tracks/*"]}
+);
